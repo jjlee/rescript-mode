5c03b1755c6b6f26b84a7bca74a4c8e180c64f5b
John Lee <jjl@pobox.com>
2021-04-28 09:42:09 +0100

    License nits

--- rescript-indent.el
+++ rescript-indent.el
@@ -10,6 +10,8 @@
 ;; Date: 2021-04-10
 ;; Keywords: languages, rescript
 
+;; This file is NOT part of GNU Emacs.
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
